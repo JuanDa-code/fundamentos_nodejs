9090c8ac6784135795b816728a4a7eb38973e121
JuanDa-code <juandax123456@gmail.com>
2023-05-10 12:33:03 -0500

    Breve explicacion de los globales

--- modulos/globales.js
+++ modulos/globales.js
@@ -1,3 +1,9 @@
 global.miVariable = 'Hola';
 
 console.log(miVariable);
+
+/* 
+Existen una cantidad de funciones y utilidades 
+que nos provee por default node.js y javascript.
+Funciones como setInterval o setImmediate.
+*/
